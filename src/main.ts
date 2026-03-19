4006d2daff79e3c0db2be21d84aa91dd7521d2f8
Tiago Celestino <tcelestino@gmail.com>
2026-03-19 17:57:45 -0300

    feat: add validate pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Shorten API')
